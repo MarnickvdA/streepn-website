5ab9ddc916aa3740a6693c5acf96524986dd0320
Marnick van der Arend <marnick@vdarend.com>
2021-10-12 16:06:15 +0200

    📓 Added blog links

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,49 +1,46 @@
-import { CUSTOM_ELEMENTS_SCHEMA, ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { StyleGuideComponent } from './components/style-guide/style-guide.component';
+import {CUSTOM_ELEMENTS_SCHEMA, ModuleWithProviders, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
 
-export const components = [
-  StyleGuideComponent
-];
+export const components = [];
 export const pipes = [];
 export const directives = [];
 
 @NgModule({
-	declarations: [
-		...components,
-		...pipes,
-		...directives,
-	],
-	imports: [
-		CommonModule,
-		FormsModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-		RouterModule,
-		TranslateModule
-	],
-	providers: [],
-	exports: [
-		CommonModule,
-		FormsModule,
-		ReactiveFormsModule,
-		HttpClientModule,
-		TranslateModule,
-		...components,
-		...pipes,
-		...directives
-	],
-	schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  declarations: [
+    ...components,
+    ...pipes,
+    ...directives,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    TranslateModule
+  ],
+  providers: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule,
+    ...components,
+    ...pipes,
+    ...directives
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule {
-	static forRoot(): ModuleWithProviders<SharedModule> {
-		return {
-			ngModule: SharedModule,
-			providers: []
-		};
-	}
+  static forRoot(): ModuleWithProviders<SharedModule> {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
 }
